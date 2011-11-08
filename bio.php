e908e47b30bfc9f3ccd370bccc77ee300496811b
Andrew Watson <andrewdavidwatson@gmail.com>
2011-11-08 09:35:54 -0500

    updated bio

--- bio.php
+++ bio.php
@@ -2,15 +2,13 @@
             <? include('header.php'); ?>
             <div class="bio">
                 <h3>About</h3>
-                <p>Andrew David Watson is a Director of Photography based in New York City. His specialty is a unique mix of stylized narrative approaches blended with a deep understanding of documentary storytelling. Andrew "shoots documentaries like narratives, and narratives like documentaries." </p>
-                <p>Andrew's work on features, non-profits, and broadcast projects has taken him across the globe; Including shooting environmental issues in Colombia, virtual video game economies in China, nightlife in Germany and educational work in New Orleans. Andrew shot the IFC's documentary series "2008 Uncut" which involved covering the Obama campaign from small town halls in New Hampshire, to traveling on the campaign plane, to election night in Chicago.</p>
-                <p>Andrew's work has been seen on IFC, TLC, MTV, NBC and has received numerous awards and grants. In addition, 
-his photography has been featured by Mother Jones and his film work has screened in over 100 film festivals worldwide.</p>
+                <p>Andrew David Watson is a Director DP based in New York City. His specialty is a unique mix of stylized narrative approaches blended with a deep understanding of documentary storytelling. Andrew shoots documentaries like narratives, and narratives like documentaries.
+ </p>
+                <p>Andrew's work on features, non-profits, and broadcast projects has taken him across the globe; Including shooting environmental issues in Colombia, virtual video game economies in China, nightlife in Germany and educational work in New Orleans.  Andrew DPed the IFC's documentary series "2008 Uncut" which involved extensive on location coverage of the 2008 election.  Andrew also DP'ed PBS / Art 21's web series "New York Up Close," profiling emerging new york city based artist.  Most recently Andrew DP'd the feature documentary CHARGE, a cinematic look at Bolivia's struggles with natural resources, shot in 4k on the RED in extremely demanding situations. </p>
+                
+                <p>Andrew's recent directing credits include a series of short films for ETSY.com, which have received hundreds of thousands of views.  In addition, Andrew's has worked as DP on various projects for PBS, TLC, NBC, MTV in additional to well know international clients such as Macy's, JWT, PUMA and The Tate Modern.  Beyond his freelance work, Andrew has taught as an adjunct professor in the journalism department at Temple University (Spring & Fall 2009).</p>
 
-                <p>Beyond his freelance work, Andrew has taught as an adjunct professor in the journalism department at Temple 
-University (Spring &amp; Fall 2009). Andrew loves anything with two wheels, whether its his touring bicycle or his 1970s motorcycle, and he is always up for an adventure!</p>
 
-<p><img src="images/screens/clients-wide.jpg" width="650"></p>
 
             <p class="contact"><b>
             Andrew David Watson | 
@@ -18,5 +16,9 @@ University (Spring &amp; Fall 2009). Andrew loves anything with two wheels, whet
             (c) 215.391.3850 |  
             (w) 347.926.3456</b>
             </p>
+            
+            <p><img src="images/screens/clients-wide.jpg" width="650"></p>
+
+            
             </div>
             <? include('footer.php'); ?>
