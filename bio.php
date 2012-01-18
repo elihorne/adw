7a2da963a91ee271e5a78265bc4b807bf233e0db
Andrew Watson <andrewdavidwatson@gmail.com>
2012-01-18 16:45:21 -0500

    images bio

--- bio.php
+++ bio.php
@@ -2,7 +2,7 @@
             <? include('header.php'); ?>
             <div class="bio">
                 <h3>About</h3>
-                <p>Andrew David Watson is a Director DP based in New York City. His specialty is a unique mix of stylized narrative approaches blended with a deep understanding of documentary storytelling. Andrew shoots documentaries like narratives, and narratives like documentaries.
+                <p>Andrew David Watson is a Director DP based in New York City. His specialty is a unique mix of stylized narrative approaches blended with a deep understanding of documentary storytelling. Andrew shoots documentaries like narratives, and narratives like documentaries.<img src="http://www.andrewdavidwatson.com/blog/wp-content/uploads/2012/01/IMG_2583.jpg" align="right" width="200">
  </p>
                 <p>Andrew's work on features, non-profits, and broadcast projects has taken him across the globe; Including shooting environmental issues in Colombia, virtual video game economies in China, nightlife in Germany and educational work in New Orleans.  Andrew DPed the IFC's documentary series "2008 Uncut" which involved extensive on location coverage of the 2008 election.  Andrew also DP'ed PBS / Art 21's web series "New York Up Close," profiling emerging new york city based artist.  Andrew is currently DPing the feature documentary CHARGE (in production), a cinematic look at Bolivia's lithium production, shot in 4k on the RED in extremely demanding situations. </p>
                 
