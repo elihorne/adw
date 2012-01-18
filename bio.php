502679c89ea3de32eb0b2f09a30a8483a5825ffb
Andrew Watson <andrewdavidwatson@gmail.com>
2012-01-18 16:32:40 -0500

    bio update

--- bio.php
+++ bio.php
@@ -6,7 +6,7 @@
  </p>
                 <p>Andrew's work on features, non-profits, and broadcast projects has taken him across the globe; Including shooting environmental issues in Colombia, virtual video game economies in China, nightlife in Germany and educational work in New Orleans.  Andrew DPed the IFC's documentary series "2008 Uncut" which involved extensive on location coverage of the 2008 election.  Andrew also DP'ed PBS / Art 21's web series "New York Up Close," profiling emerging new york city based artist.  Andrew is currently DPing the feature documentary CHARGE (in production), a cinematic look at Bolivia's lithium production, shot in 4k on the RED in extremely demanding situations. </p>
                 
-                <p>Andrew's recent directing credits include a series of short films for ETSY.com, which have received hundreds of thousands of views.  In addition, Andrew has worked as DP on various projects for PBS, TLC, NBC, MTV in additional to well know international clients such as Macy's, JWT, PUMA and The Tate Modern.  Beyond his freelance work, Andrew has taught as an adjunct professor in the journalism department at Temple University (Spring & Fall 2009) and loves motorcycles.</p>
+                <p>Andrew's recent directing credits include a series of short films for ETSY.com, which have received hundreds of thousands of views.  In addition, Andrew has worked as DP on various projects for PBS, TLC, NBC, MTV in additional to well know international clients such as Macy's, JWT, PUMA and The Tate Modern.  Beyond his freelance work, Andrew has taught as an adjunct professor in the Journalism department at Temple University (Spring & Fall 2009) and loves motorcycles.</p>
 
 
 
