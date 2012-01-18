c2e924075819d7061653aff2437fda675f62f256
Andrew Watson <andrewdavidwatson@gmail.com>
2012-01-18 17:10:02 -0500

    bio page

--- bio.php
+++ bio.php
@@ -2,9 +2,12 @@
             <? include('header.php'); ?>
             <div class="bio">
                 <h3>About</h3>
-                <p><img src="http://www.andrewdavidwatson.com/images/bio-camera.jpg" align="right" width="250" align="top" border="0">Andrew David Watson is a Director DP based in New York City. His specialty is a unique mix of stylized narrative approaches blended with a deep understanding of documentary storytelling. Andrew shoots documentaries like narratives, and narratives like documentaries.
+                <p><img src="http://www.andrewdavidwatson.com/images/bio-camera.jpg" align="right" width="300" align="top" border="0">Andrew David Watson is a Director DP based in New York City. His specialty is a unique mix of stylized narrative approaches blended with a deep understanding of documentary storytelling. Andrew shoots documentaries like narratives, and narratives like documentaries.
                  </p>
                 <p>Andrew's work on features, non-profits, and broadcast projects has taken him across the globe; Including shooting environmental issues in Colombia, virtual video game economies in China, nightlife in Germany and educational work in New Orleans.  Andrew DPed the IFC's documentary series "2008 Uncut" which involved extensive on location coverage of the 2008 election.  Andrew also DP'ed PBS / Art 21's web series "New York Up Close," profiling emerging new york city based artist.  Andrew is currently DPing the feature documentary CHARGE (in production), a cinematic look at Bolivia's lithium production, shot in 4k on the RED in extremely demanding situations. </p>
+                
+                <p><img class="shadowbox" src="images/screens/clients-wide.jpg" width="650"></p>
+            
                 
                 <p>Andrew's recent directing credits include a series of short films for ETSY.com, which have received hundreds of thousands of views.  In addition, Andrew has worked as DP on various projects for PBS, TLC, NBC, MTV in additional to well know international clients such as Macy's, JWT, PUMA and The Tate Modern.  Beyond his freelance work, Andrew has taught as an adjunct professor in the Journalism department at Temple University (Spring & Fall 2009) and loves motorcycles.</p>
 
@@ -17,7 +20,6 @@
             (w) 347.926.3456</b>
             </p>
             
-            <p><img class="shadowbox" src="images/screens/clients-wide.jpg" width="650"></p>
             
             </div>
           
