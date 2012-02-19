43557399cd863827542f30246feef8f5174e0957
Andrew Watson <andrewdavidwatson@gmail.com>
2012-02-19 17:31:16 -0500

    bio update

--- bio.php
+++ bio.php
@@ -11,17 +11,17 @@
                 
                 <p>
 
-<p>- I'm a NYC based Director / DP with an emphasize on creative nonfiction, with an overlap into commercial and narrative. </p>
+<p>- I'm a NYC based Director / DP with an emphasis on creative nonfiction, with an overlap into commercial and narrative </p>
 
 <p>- I love anything and everything with two wheels, especially vintage motorcycles.  It keeps me a kid at heart.  </p>
 
-<p>- By this point I have shot in over 10 countries, the highlights being Bolivia, Colombia and China.  I have travel to upwards of 20. </p>
+<p>- My work has taken me to such distinct locales as Bolivia, Colombia and China.  To date I have travelled to upwards of 20 countries. </p>
 
-<p>- I define my approach to lighting as naturalistic and minimalistic. My style is further defined by framing and movement, with a strong connection to still photography.  </p>
+<p>- My approach to lighting can best be defined as naturalistic and minimalistic. My style is further defined by its framing, movement, and strong connection to still photography.  </p>
 
-<p>- I covered the 2008 election for a doc series for the IFC.  I got to hang out on Obama's plane and drive in a motorcade.  Pretty awesome.  </p>
+<p>- I covered the 2008 election for a documentary series ("2008 Uncut") for the IFC.  I hung out on the plane Obama used during his campaign  and was also at one point in his motorcade.  Pretty awesome.  </p>
 
-<p>- I have worked with crews as small as just a producer and as big as having 10+ clients watching in video village. Not to mention art department, G&E and the rest. </p>
+<p>- I have worked with crews of all shapes and sizes: from just a producer to 10+ clients watching in video village -- not to mention the art department, G&E and all the rest. </p>
 
 <p>- I have been bitten by fire ants while shooting on a coco farm in the Amazon basin, and have the scars to prove it.  </p>
 
@@ -33,9 +33,9 @@
 
 <p>- Did you know a 2000 watt generator will not power a 800watt HMI at 14,000 feet above sea level?  Ask me about it, its an interesting story. </p>
 
-<p>- I wasn't on a plane until I was 21.  Since I have racked up a ridiculous amount of miles, recognize staff at JFK, have elite status with Delta and want to move to Amsterdam partly because I love Schiphol Airport!  </p>
+<p>- I didn’t fly on a plane until I was 21.  Since then I have racked up a ridiculous amount of miles; recognize staff at JFK; have elite status with Delta and want to move to Amsterdam partly because I love Schiphol (spelling?) Airport!  </p>
 
-<p>- Need a great gaffer in Dallas? Got that.  Need an awesome sound man in DC? Got that too. By this point I have worked in a majority of the country.  </p>
+<p>- Need a great gaffer in Dallas? Got that.  Need an awesome sound man in DC? Got that, too. By this point I have worked in most major US metropiltan areas.  </p>
 
 <p>- I have shot on the RED, Sony F3, Canon C300, HDSLRS, Varicam and most other cameras.</p>
 
