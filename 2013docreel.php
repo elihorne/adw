196a7d329743ce88f9e4e8959613912b2fa49f69
Andrew Watson <andrewdavidwatson@gmail.com>
2013-01-17 18:25:24 -0500

    size

--- 2013docreel.php
+++ 2013docreel.php
@@ -2,11 +2,13 @@
             <? include('header.php'); ?>
             <div>
                 <h3>2013 Creative Doc Reel</h3>
+               </div>
                
+               <div class="catagories">
                 
                 
 
-<iframe src="http://player.vimeo.com/video/56958533?title=0&amp;byline=0&amp;portrait=0" width="800" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe> <p><a href="http://vimeo.com/56958533">2013 Documentary Reel</a> from <a href="http://vimeo.com/andrewdavidwatson">Andrew David Watson</a> on <a href="http://vimeo.com">Vimeo</a>.</p>
+<iframe src="http://player.vimeo.com/video/56958533?title=0&amp;byline=0&amp;portrait=0" width="960" width="640" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe> <p><a href="http://vimeo.com/56958533">2013 Documentary Reel</a> from <a href="http://vimeo.com/andrewdavidwatson">Andrew David Watson</a> on <a href="http://vimeo.com">Vimeo</a>.</p>
 
 								<ul class="payload">
                                     <li>
