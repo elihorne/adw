2714bcade3f3d7ee902fc3137b5f4e83f3fd35ef
Andrew Watson <andrewdavidwatson@gmail.com>
2013-01-17 18:31:44 -0500

    git pull

--- 2013docreel.php
+++ 2013docreel.php
@@ -1,15 +1,13 @@
             <? $section = 'bio-contact'; ?>
             <? include('header.php'); ?>
         
-               
-               <div>
-                
-                
+			<div>
 
-<iframe src="http://player.vimeo.com/video/56958533?title=0&amp;byline=0&amp;portrait=0" width="1000" height="562" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>			
+				<iframe src="http://player.vimeo.com/video/56958533?title=0&amp;byline=0&amp;portrait=0" 
+				width="1000" height="562" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>			
 						
-						       <h3>2013 Creative Doc Reel</h3>
-						       <h4>andrew david watson</h4>
+					<b>2013 Creative Doc Reel</b><br>
+						Andrew David Watson
 				
             </div>
           
