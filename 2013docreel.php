93231fab53ee7be343205179e3b334c257213dcc
Andrew Watson <andrewdavidwatson@gmail.com>
2013-01-17 18:21:51 -0500

    individual pages

---
+++ 2013docreel.php
@@ -0,0 +1,27 @@
+            <? $section = 'bio-contact'; ?>
+            <? include('header.php'); ?>
+            <div class="bio">
+                <h3>2013 Creative Doc Reel</h3>
+                <p><img class="shadowbox" src="http://www.andrewdavidwatson.com/images/bio-camera.jpg" align="right" width="350" align="top" hspace="10"></p>
+                
+                
+
+<iframe src="http://player.vimeo.com/video/56958533?title=0&amp;byline=0&amp;portrait=0" width="500" height="281" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe> <p><a href="http://vimeo.com/56958533">2013 Documentary Reel</a> from <a href="http://vimeo.com/andrewdavidwatson">Andrew David Watson</a> on <a href="http://vimeo.com">Vimeo</a>.</p>
+
+								<ul class="payload">
+                                    <li>
+                                        <span class="type">2011 Creative Demo Reel</span>
+                                    </li>
+                                    <li>
+                                        <span class="type">Roles:</span> Director of Photography
+                                    </li>
+                                    <li>
+                                        <span class="type">Date:</span> November, 2011
+                                    </li>
+                                </ul>
+                                
+                                
+            
+            </div>
+          
+            <? include('footer.php'); ?>
