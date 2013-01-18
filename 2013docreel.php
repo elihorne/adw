ada978aeacd380f5829136ab9074f79bb119ca61
Andrew Watson <andrewdavidwatson@gmail.com>
2013-01-17 19:14:51 -0500

    text

--- 2013docreel.php
+++ 2013docreel.php
@@ -5,9 +5,7 @@
 
 				<iframe src="http://player.vimeo.com/video/56958533?title=0&amp;byline=0&amp;portrait=0" 
 				width="720" height="405" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>			
-				</div>
-				
-				<div class="vimeotext">		
+					
 					<b>2013 Creative Doc Reel</b><br>
 						Andrew David Watson 
 				
