93f31214c13d294bd7a1f570052eb33f21a65861
Andrew Watson <andrewdavidwatson@gmail.com>
2013-01-17 19:06:57 -0500

    align

--- 2013docreel.php
+++ 2013docreel.php
@@ -1,13 +1,13 @@
             <? $section = 'bio-contact'; ?>
             <? include('header.php'); ?>
         
-			<div float="left">
+			<div class="vimeo">
 
 				<iframe src="http://player.vimeo.com/video/56958533?title=0&amp;byline=0&amp;portrait=0" 
-				width="640" height="320" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>			
+				width="960" height="540" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>			
 				</div>
 				
-				<div float="right"></div>		
+				<div class="vimeotext"></div>		
 					<b>2013 Creative Doc Reel</b><br>
 						Andrew David Watson 
 				
