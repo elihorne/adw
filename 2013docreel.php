270b5d443798be978495370bffd3fd3809b6fc1a
Andrew Watson <andrewdavidwatson@gmail.com>
2013-01-18 12:14:02 -0500

    Revert "align"
    This reverts commit 93f31214c13d294bd7a1f570052eb33f21a65861.

--- 2013docreel.php
+++ 2013docreel.php
@@ -1,13 +1,13 @@
             <? $section = 'bio-contact'; ?>
             <? include('header.php'); ?>
         
-			<div class="vimeo">
+			<div float="left">
 
 				<iframe src="http://player.vimeo.com/video/56958533?title=0&amp;byline=0&amp;portrait=0" 
-				width="960" height="540" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>			
+				width="640" height="320" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>			
 				</div>
 				
-				<div class="vimeotext"></div>		
+				<div float="right"></div>		
 					<b>2013 Creative Doc Reel</b><br>
 						Andrew David Watson 
 				
