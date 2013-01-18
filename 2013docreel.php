ea3147f8feea712326cdc60baea9c419f5b182c5
Andrew Watson <andrewdavidwatson@gmail.com>
2013-01-18 12:14:02 -0500

    Revert "limited text"
    This reverts commit 588d1ddf3f4a38812edf1e2ccf02e4339e2942b2.

--- 2013docreel.php
+++ 2013docreel.php
@@ -1,15 +1,16 @@
             <? $section = 'bio-contact'; ?>
             <? include('header.php'); ?>
-        
+            <div>
+                <h3>2013 Creative Doc Reel</h3>
+               </div>
                
-               <div>
+               <div class="catagories">
                 
                 
 
-<iframe src="http://player.vimeo.com/video/56958533?title=0&amp;byline=0&amp;portrait=0" width="1000" height="562" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>			
+<iframe src="http://player.vimeo.com/video/56958533?title=0&amp;byline=0&amp;portrait=0" width="960" height="540" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>			
 						
-						       <h3>2013 Creative Doc Reel</h3>
-						       <h4>andrew david watson</h4>
+						
 				
             </div>
           
