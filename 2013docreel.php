0417ac8657b6b6096bcc287447b2d00c3b4a1c17
Andrew Watson <andrewdavidwatson@gmail.com>
2013-01-18 12:14:02 -0500

    Revert "git pull"
    This reverts commit 2714bcade3f3d7ee902fc3137b5f4e83f3fd35ef.

--- 2013docreel.php
+++ 2013docreel.php
@@ -1,13 +1,15 @@
             <? $section = 'bio-contact'; ?>
             <? include('header.php'); ?>
         
-			<div>
+               
+               <div>
+                
+                
 
-				<iframe src="http://player.vimeo.com/video/56958533?title=0&amp;byline=0&amp;portrait=0" 
-				width="1000" height="562" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>			
+<iframe src="http://player.vimeo.com/video/56958533?title=0&amp;byline=0&amp;portrait=0" width="1000" height="562" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>			
 						
-					<b>2013 Creative Doc Reel</b><br>
-						Andrew David Watson
+						       <h3>2013 Creative Doc Reel</h3>
+						       <h4>andrew david watson</h4>
 				
             </div>
           
