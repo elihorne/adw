76139b67326a7711ed9f5b35a0f5c9c09b81d60c
Andrew Watson <andrewdavidwatson@gmail.com>
2013-01-17 19:16:20 -0500

    fda

--- 2013docreel.php
+++ 2013docreel.php
@@ -5,9 +5,22 @@
 
 				<iframe src="http://player.vimeo.com/video/56958533?title=0&amp;byline=0&amp;portrait=0" 
 				width="720" height="405" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>			
+				
 					
-					<b>2013 Creative Doc Reel</b><br>
-						Andrew David Watson 
+					         <li>
+                         		  <ul class="payload">
+                                    <li>
+                                        <span class="type">2011 Creative Demo Reel</span>
+                                    </li>
+                                    <li>
+                                        <span class="type">Roles:</span> Director of Photography
+                                    </li>
+                                    <li>
+                                        <span class="type">Date:</span> November, 2011
+                                    </li>
+                                </ul>
+                            </a> 
+                       </li>
 				
             </div>
           
