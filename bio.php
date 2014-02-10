6d1a5e77d64bf876ba16d00a320fd509298c5264
Andrew Watson <andrewdavidwatson@gmail.com>
2014-02-10 15:16:22 -0500

    git pull

--- bio.php
+++ bio.php
@@ -7,19 +7,19 @@
                 
     <div class="biobody">           
                 
-List are more fun then awkward third person bios, so here are some interesting facts:<br><br>
+List are more fun then awkward third person bios, so here are some interesting facts:<br>
 
-<br>- I'm a NYC based Director / DP.  My directional focus is creative nonfiction, while my DP focus is anything I can make beautiful.<br>
+<br>- I'm a NYC based Director / DP.  My directional focus is creative non-fiction, while my DP focus is anything I can make beautiful.<br>
 
 <br>- I love anything and everything with two wheels, especially vintage motorcycles and steel framed bicycles. It keeps me a kid at heart.<br>
 
-<br>- My work has taken me to such distinct locales as Bolivia, Colombia, Haiti, South Africa, China, Japan, Korea, Hong Kong, Philippines, France, UK, Germany, Costa Rica... you get the picture.  In fact, you can see the pictures here on my <a href="http://www.andrewdavidwatson.com/blog">Blog!</a>.<br>
+<br>- My work has taken me to such distinct locales as Bolivia, Colombia, Haiti, South Africa, China, Japan, Korea, Hong Kong, Philippines, France, UK, Germany, Costa Rica... you get the picture.  In fact, you can <i>even see</i> the pictures here on my <a href="http://www.andrewdavidwatson.com/blog">travel blog!</a>.<br>
 
 <br>- I have worked with agencies such as: KBS+, MRY, JWT, Draft FCB, PUBLICIS, P+W, <br>
 
 <br>- I'm as comfortable working in the wilderness with a small crew as I am in a midtown studio packed full of agency folks. <br>
 
-<br>- Personal projects of mine have received combined views of over a million, while also screening in various festivals and picking up Vimeo Staff picks along the way. <br>
+<br>- My personal work has received combined views of over a million, while also screening in various festivals and picking up <a href="https://vimeo.com/78068001">Vimeo Staff</a> picks along the way. <br>
 
 <br>- I have a bunch of random credits on networks such as PBS, Showtime, BBC, IFC, TLC.... Some clients and brands I have shot for include: The New Yorker, ETSY, Red Wing Heritage, Macy's, Puma, Tate Modern. <br>
 
