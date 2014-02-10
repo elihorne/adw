24486b0f5a4ab00ffac8a45a1ee0c912726be4c7
Andrew Watson <andrewdavidwatson@gmail.com>
2014-02-10 15:09:01 -0500

    bio updates

--- bio.php
+++ bio.php
@@ -9,22 +9,22 @@
                 
 List are more fun then awkward third person bios, so here are some interesting facts:<br><br>
 
-<br>- I'm a NYC based Director / DP.  My directional focus is creative nonfiction, while my DP focus is anything I can make beautiful.
+<br>- I'm a NYC based Director / DP.  My directional focus is creative nonfiction, while my DP focus is anything I can make beautiful.<br><br>
 
-<br>- I love anything and everything with two wheels, especially vintage motorcycles and steel framed bicycles. It keeps me a kid at heart.
+<br>- I love anything and everything with two wheels, especially vintage motorcycles and steel framed bicycles. It keeps me a kid at heart.<br><br>
 
-<br>- My work has taken me to such distinct locales as Bolivia, Colombia, Haiti, South Africa, China, Japan, Korea, Hong Kong, Philippines, France, UK, Germany, Costa Rica... you get the picture.  In fact, you can see the pictures here on my <a href="http://www.andrewdaviwatson.com/blog">Blog!</a>.
+<br>- My work has taken me to such distinct locales as Bolivia, Colombia, Haiti, South Africa, China, Japan, Korea, Hong Kong, Philippines, France, UK, Germany, Costa Rica... you get the picture.  In fact, you can see the pictures here on my <a href="http://www.andrewdavidwatson.com/blog">Blog!</a>.<br><br>
 
-<br>- I have worked with agencies such as: KBS+, JWT, Draft FCB, Publicies, P+W, 
+<br>- I have worked with agencies such as: KBS+, JWT, Draft FCB, Publicies, P+W, <br><br>
 
-<br>  I'm as comfortable working in the wilderness with a small crew as I am in a midtown studio packed full of agency folks. 
+<br>  I'm as comfortable working in the wilderness with a small crew as I am in a midtown studio packed full of agency folks. <br><br>
 
-<br> - Personal projects of mine have received combined views of over a million, while also screening in various festivals and picking up Vimeo Staff picks along the way. 
+<br> - Personal projects of mine have received combined views of over a million, while also screening in various festivals and picking up Vimeo Staff picks along the way. <br><br>
 
-<br> I have a bunch of random credits on networks such as PBS, Showtime, BBC, IFC, TLC.... Some clients and brands I have shot for include: The New Yorker, ETSY, Red Wing Heritage, Macy's, Puma, Tate Modern, PETA
+<br> I have a bunch of random credits on networks such as PBS, Showtime, BBC, IFC, TLC.... Some clients and brands I have shot for include: The New Yorker, ETSY, Red Wing Heritage, Macy's, Puma, Tate Modern, PETA<br><br>
 
 <br>- Did you know a 2000 watt generator will not power a 800watt HMI at 14,000 feet above sea level? Ask me about it, its an interesting story.
-
+<br>
    
 <p> <p></p> </p>   
                     
